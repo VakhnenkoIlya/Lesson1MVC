db3ef167a961bf3a78769bfa57c1d097af1de947
VakhnenkoIlya <81589453+VakhnenkoIlya@users.noreply.github.com>
2022-04-11 14:57:07 +0200

    HW
    не понял как сделать 4-е задание, нужно было результаты метода фибоначи записывать в список?

---
+++ Lesson1.1/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Stopwatch sw = new Stopwatch();
+            sw.Restart();
+            Console.WriteLine($"{sw.Elapsed}");
+            Console.ReadLine();
+            var date1 = new DateTime();
+            Console.WriteLine($"{date1.Second}");
+        }
+    }
+}
+
---
+++ Lesson1.1/Lesson1.1/MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using Newtonsoft.Json.Serialization;
+using NPOI.SS.Formula.Functions;
+using System;
+using System.Threading;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace Lesson1._1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Thread thread;
+        public MainWindow()
+        {
+            InitializeComponent();
+            thread = new(Fibonachi);
+            thread.Start();
+
+        }
+
+        public void Fibonachi()
+        {
+            try
+            {
+                int prev = 1;
+                int current = 1;
+                int temp;
+                while (current < 6000)
+                {
+                    Application.Current.Dispatcher.Invoke(DispatcherPriority.Background,
+                    new Action(() =>
+                    {
+                        TextBox1.Text = current.ToString();
+                        temp = current;
+                        current = current + prev;
+                        prev = temp;
+                        Application.Current.Dispatcher.Invoke(DispatcherPriority.ContextIdle,
+                        new Action(() =>
+                        {
+                            Regulate();
+                        }));
+                    }));
+                }
+            }
+            catch (ThreadInterruptedException e)
+            {
+                MessageBox.Show($"{e}");
+            }
+        }
+
+        private void Regulate()
+        {
+
+            Application.Current.Dispatcher.Invoke(DispatcherPriority.ContextIdle,
+            new Action(() =>
+            {
+                TimeTextBox.Text = "1";
+
+            }));
+            Thread.Sleep(1000);
+            Application.Current.Dispatcher.Invoke(DispatcherPriority.ContextIdle,
+            new Action(() =>
+            {
+                TimeTextBox.Text = "2";
+
+            }));
+            Thread.Sleep(1000);
+            Application.Current.Dispatcher.Invoke(DispatcherPriority.ContextIdle,
+            new Action(() =>
+            {
+                TimeTextBox.Text = "3";
+            }));
+            Thread.Sleep(1000);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            thread.Interrupt();
+        }
+    }
+}
+
+
+
